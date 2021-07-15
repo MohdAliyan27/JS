b72fe008a3dcc78efe4c2aad77e29d698e15090b
MohdAliyan27 <71941136+MohdAliyan27@users.noreply.github.com>
2021-07-15 23:21:17 +0530

    Added

---
+++ tryCatch.js
@@ -0,0 +1,12 @@
+try {
+    let a=10,b=0;
+    let c=a/b;
+    console.log(c);
+    //console1.log(d);
+    //console1.log(c);
+    console.log("Hey");
+    
+}
+   catch(err) {
+    console.log(err.message);
+}
