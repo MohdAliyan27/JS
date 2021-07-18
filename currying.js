670a4284d45f0b9bcf233bcfc27f756e816f7a10
MohdAliyan27 <71941136+MohdAliyan27@users.noreply.github.com>
2021-07-19 00:46:43 +0530

    Update currying.js

--- currying.js
+++ currying.js
@@ -1,15 +1,5 @@
 //Currying
 
-function volume(length) {
-      return (width) => {
-         return (height) => {
-            return height * width * length;
-         }
-      }
-}
-let vol= volume(1)(2)(4);
-console.log(vol);
-
 //Type 1 currying by bind method
 let multiply1=function(x,y){
     return x*y;
@@ -18,7 +8,21 @@ let multiply1=function(x,y){
 let multByTwo1 = multiply1.bind(this,2);
 console.log(multByTwo1(6));
 
+// Type 2 starts from her.
+function volume(length) {
+      return (width) => {
+         return (height) => {
+            return height * width * length;
+         }
+      }
+}
+
+let vol= volume(1)(2)(4);
+console.log(vol);
+
+
 //Type 2 By Function Closure.
+//
 let multiply2 = function(x){
     return function (y){
         console.log(x*y);
