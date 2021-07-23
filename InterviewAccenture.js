33bdc8c0190cb3107f6c9d827464f475d7dc375e
MohdAliyan27 <71941136+MohdAliyan27@users.noreply.github.com>
2021-07-23 17:45:30 +0530

    Accenture.js

---
+++ InterviewAccenture.js
@@ -0,0 +1,39 @@
+//ist program
+
+setTimeout(() => {
+    console.log("Hi")
+}, 5000);
+
+//2nd program
+function greeting(){
+    console.log("Hello");
+}
+
+greeting();
+
+//3rd proram
+var num =[1,2,3,4,5,6,7,8]
+var even = num.filter((element) => {
+    if(element%2 ==0)
+    {
+     return element;
+    }
+})
+console.log(even);
+
+
+//4th program
+
+var fs= require('fs');
+fs.readFile('fileRead.txt',(err,data) =>{
+    if(err){
+        console.log(err.stack);
+        return;
+    }
+    else{
+        console.log(data.toString());
+    }
+    
+});
+
+
