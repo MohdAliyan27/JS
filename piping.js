d4a3c6f27ede9bf0248eadc50552a732852d5d82
MohdAliyan27 <71941136+MohdAliyan27@users.noreply.github.com>
2021-07-25 18:22:30 +0530

    Updated

--- piping.js
+++ piping.js
@@ -4,3 +4,6 @@ var readable = fs.createReadStream('file1.txt');
 var writable = fs.createWriteStream('file2.txt');
 
 readable.pipe(writable);
+
+//we need to create two text files file1 and file2 file1 contains important we need to duplicate into file2. And file2 is empty after the operation file2 
+//also contains data that was in file1.
