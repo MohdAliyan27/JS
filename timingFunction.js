64044af02603b46e804ed7129f32e1bf3fd71746
MohdAliyan27 <71941136+MohdAliyan27@users.noreply.github.com>
2021-07-28 13:22:03 +0530

    Update timingFunction.js

--- timingFunction.js
+++ timingFunction.js
@@ -1,15 +1,20 @@
 //Set Time out
-
+//after a fixed amount of time say 5000 millisecond the code executes. 
 console.log("First");
 setTimeout(()=>{
     console.log("Second");
 },0)
 console.log("Third");
 
-//Set Immediate
+//Set Immediate 
+//will execute after current event loop cycle.
 
 console.log("Ist");
 setImmediate(() =>{
     console.log('IInd');
 })
 console.log("IIIrd");
+
+//set Interval used to execute code multiple times.
+
+//process.nextTick()
