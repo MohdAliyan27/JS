24c075b27b221a2da3401e62d45184e3e0fe0202
MohdAliyan27 <71941136+MohdAliyan27@users.noreply.github.com>
2021-07-29 19:42:23 +0530

    Add files via upload

--- de-structuring.js
+++ de-structuring.js
@@ -18,3 +18,27 @@ console.log(name,age);
 const arr =['qwerty','Aligarh','Agra']
 const [abc,bc] = arr;
 console.log(abc,bc);
+
+let person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    middleName: 'C.'
+    //currentAge: 28
+};
+
+let { firstName,  lastName, middleName = '', currentAge: age_ = 18 } = person;
+// middleName = ''  //default value.
+// currentAge: age = 18 //if no value is supplied then this works
+
+console.log(middleName); // 'C.'
+console.log(age_); // 28
+
+//extra
+let person2 = {
+    firstName: 'John',
+    lastName: 'Doe'
+};
+
+let {firstName :f,lastName :l} = person2;
+
+console.log("my name is "+f +l)
