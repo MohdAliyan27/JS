dbcaf8b1965903d98bacdb6b0bc169e69313285c
MohdAliyan27 <71941136+MohdAliyan27@users.noreply.github.com>
2021-07-30 20:48:10 +0530

    Add files via upload

--- closureFunction.js
+++ closureFunction.js
@@ -1,10 +1,13 @@
 // closure means inner function always have access to the outer functions variable and parameters.
+var z=3;// inner can access the global variable
 function outer(){
-    var outer_variable=27;
+    var outer_variable=27; // inner can access outer variable
     function inner(){
-        console.log(outer_variable)
+        var inner=2; // inner can access its own variable
+        console.log((inner * outer_variable)/z)
     }
     inner()
 }
 
 outer()
+
