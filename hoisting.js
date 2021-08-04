d27cd85ab1f05c3834047538dfe5cc87e5d483af
MohdAliyan27 <71941136+MohdAliyan27@users.noreply.github.com>
2021-08-04 23:59:19 +0530

    Update hoisting.js

--- hoisting.js
+++ hoisting.js
@@ -1,3 +1,18 @@
-console.log(`Hello World ${str} welcome here.`);
+// console.log(`Hello World ${str} welcome here.`);
 
-let str='Node.js';
+// let str='Node.js';
+
+x=2;
+console.log(x)
+var x;
+
+carName = "Volvo";
+console.log(carName) // reference error
+let carName;
+
+var x = 5; // Initialize x
+
+console.log(x +" "+ y)  // Display x and y
+         
+
+var y = 7; // Initialize y
