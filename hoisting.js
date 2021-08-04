aa20f9a8693b8de6a35e84dee6448a0fdff627fc
MohdAliyan27 <71941136+MohdAliyan27@users.noreply.github.com>
2021-08-05 00:05:06 +0530

    Update hoisting.js

--- hoisting.js
+++ hoisting.js
@@ -1,21 +1,21 @@
 // Hoisting is JavaScript's default behavior of moving all declarations to the top of the current scope 
-//str='Node.js'
-//console.log(`Hello World ${str} welcome here.`);
-// var str;
+str='Node.js'
+console.log(`Hello World ${str} welcome here.`);
+var str;
 
 x=2;
 console.log(x)
 var x;
 
-carName = "Volvo";
-console.log(carName) // reference error
-let carName;
+// carName = "Volvo";
+// console.log(carName) // reference error
+// let carName;
 
 var x = 5; // Initialize x
 console.log(x +" "+ y)  // Display x and y  
 var y = 7; // Initialize y here
 //SIMILAR to show hoisting
-var x=5;
-var y;
-console.log(x +" "+ y)  // Display x and y  as undefined 
-y = 7;
+var w=5;
+var q;
+console.log(w +" "+ q)  // Display x and y  as undefined 
+q = 7;
