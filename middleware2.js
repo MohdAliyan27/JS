f665cb7834b798a5527133a0105fa69c9575d7ec
MohdAliyan27 <71941136+MohdAliyan27@users.noreply.github.com>
2021-08-05 23:15:07 +0530

    Add files via upload

---
+++ middleware2.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+
+var requestTime = function (req, res, next) {
+    const d = new Date();
+  req.requestTime = d.getMonth()
+  next()
+}
+
+app.use(requestTime)
+
+app.get('/', function (req, res) {
+  var responseText = 'Hello World!<br>'
+  responseText += '<small>Requested at: ' + req.requestTime + '</small>'
+  res.send(responseText)
+})
+
+app.listen(3000)
