aa13807f8b95c929244713b515a77843025474fd
MohdAliyan27 <71941136+MohdAliyan27@users.noreply.github.com>
2021-08-11 23:42:38 +0530

    Create http_Method.js

---
+++ http_Method.js
@@ -0,0 +1,29 @@
+//Dependencies
+var http= require('http');
+var url = require('url')
+
+var server = http.createServer((req,res) => {
+
+    //get the url and parse it
+
+    var parsedUrl = url.parse(req.url, true)
+
+    //get the path from the url
+    var path =parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g,'')
+
+    //getting the method
+
+    var method = req.method.toLowerCase();
+    //send the response
+    res.end("Hey Node")
+    console.log("method called is " + method)
+
+
+    //log the req path
+    console.log('path is '+ trimmedPath)
+})
+
+server.listen(9000, ()=>{
+    console.log("server running at 9000")
+})
