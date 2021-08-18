9a4088f9d288104b9141a346cc1483595a322a1b
MohdAliyan27 <71941136+MohdAliyan27@users.noreply.github.com>
2021-08-18 23:14:06 +0530

    Add files via upload

--- timingFunction.js
+++ timingFunction.js
@@ -1,20 +1,11 @@
-//Set Time out
-//after a fixed amount of time say 5000 millisecond the code executes. 
-console.log("First");
-setTimeout(()=>{
-    console.log("Second");
-},0)
-console.log("Third");
-
-//Set Immediate 
-//will execute after current event loop cycle.
+//console.log("First");
+// setTimeout(()=>{
+//     console.log("Second");
+// },0)
+//console.log("Third");
 
 console.log("Ist");
 setImmediate(() =>{
     console.log('IInd');
 })
 console.log("IIIrd");
-
-//set Interval used to execute code multiple times.
-
-//process.nextTick()
