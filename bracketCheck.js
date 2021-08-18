1ad9eb7a6eeee810d052a521ef1020e745689dbe
MohdAliyan27 <71941136+MohdAliyan27@users.noreply.github.com>
2021-08-18 23:12:28 +0530

    Nagarro

--- bracketCheck.js
+++ bracketCheck.js
@@ -1,54 +1,27 @@
-function areBracketsBalanced(expr)
-{ 
-    let stack = [];
- 
-    // Traversing the Expression
-    for(let i = 0; i < expr.length; i++)
-    {
-        let x = expr[i];
- 
-        if (x == '(' || x == '[' || x == '{')
-        {     
-            // Push the element in the stack
+function bracketCheck(expr){
+
+    let stack=[];
+    for (let i=0; i< expr.length ; i++){
+        let x= expr[i];
+        if(x=='('|| x=='{' || x=='[')
+        {
             stack.push(x);
             continue;
         }
-        // If current character is not opening bracket, then it must be closing.
-        // So stack cannot be empty at this point.
-        if (stack.length == 0)
-            return false;
-             
-    let check;
-    switch (x){
-        case ')':
-            check = stack.pop();
-            if (check == '{' || check == '[')
-                return false;
-            break;
- 
-        case '}':
-            check = stack.pop();
-            if (check == '(' || check == '[')
-                return false;
-            break;
- 
-        case ']':
-            check = stack.pop();
-            if (check == '(' || check == '{')
-                return false;
-            break;
+        if(stack.length ==0)
+        return false;
+
+        let check;
+        switch (x){
+            case ')':
+                check
         }
+
     }
- 
-    // Check Empty Stack
-    return (stack.length == 0);
 }
- 
-// Driver code
-let expr = "[([{}])]";
- 
-// Function call
-if (areBracketsBalanced(expr))
-    console.log("Balanced ");
+
+let expr='[{()}}]'
+if(bracketCheck(expr))
+    console.log("Balanced");
 else
-console.log("Not Balanced ");
+    console.log("Not Balanced");
