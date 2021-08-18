6b26246628eab1a9e965ce33625fd8036e14fe39
MohdAliyan27 <71941136+MohdAliyan27@users.noreply.github.com>
2021-08-18 23:15:39 +0530

    Update apply.js

--- apply.js
+++ apply.js
@@ -7,7 +7,7 @@ let printName = function(organisation, years){
     console.log("Full Name is "+" "+this.fname+" "+this.lname+" working with "+organisation+" for "+years+"yrs");
 }
 
-printName.apply(name,["TCS",5])
+printName.apply(name,["TCS",5]);
 
 let name2 ={
     fname:'Mohd',
@@ -15,4 +15,4 @@ let name2 ={
 }
 
 // We are passing the values as an ARRAY List
-printName.apply(name2,["Wipro",6])
+printName.apply(name2,["Wipro",6]);
