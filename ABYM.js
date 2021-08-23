bf439454c7b1097fb0ce5f81d3ef203c5975afb8
MohdAliyan27 <71941136+MohdAliyan27@users.noreply.github.com>
2021-08-24 01:01:31 +0530

    Update ABYM.js

--- ABYM.js
+++ ABYM.js
@@ -1,5 +1,7 @@
 //code for all the http request.
 
 //Get, post, put, delete.
+//callback
+//post
 
 //https://www.freecodecamp.org/news/learn-node-js-with-brigadier-fluffykins-part-iii-request-object-configure-routes-serve-files-7666f783dc10/
