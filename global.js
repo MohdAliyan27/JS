2df0d3a372a77f5aa210837b4f837b636f930e0c
MohdAliyan27 <71941136+MohdAliyan27@users.noreply.github.com>
2021-08-25 23:40:29 +0530

    Create global.js

---
+++ global.js
@@ -0,0 +1,12 @@
+console.log("hey"); //this is global
+
+setTimeout(()=>{
+};
+           
+clearTimeout()
+
+setInterval
+clearInterval
+
+var message = "hey";
+console.log(global.message);// undefined.
