040149704b17b24a913788d7f3d8c9d274b35b6b
MohdAliyan27 <71941136+MohdAliyan27@users.noreply.github.com>
2021-08-25 23:53:42 +0530

    Update ABYM.js

--- ABYM.js
+++ ABYM.js
@@ -6,4 +6,5 @@
 
 //https://www.freecodecamp.org/news/learn-node-js-with-brigadier-fluffykins-part-iii-request-object-configure-routes-serve-files-7666f783dc10/
 
-event bubbling
+//event bubbling
+//event emitter
