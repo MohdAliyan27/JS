425de98c40e32c9cb6265fba86f807ba27739d9e
MohdAliyan27 <71941136+MohdAliyan27@users.noreply.github.com>
2021-08-25 23:41:45 +0530

    Update global.js

--- global.js
+++ global.js
@@ -10,3 +10,5 @@ clearInterval
 
 var message = "hey";
 console.log(global.message);// undefined.
+//global means this message will be accessed anywhere. outside this file also
+// message is onlu accessible only in global.js
