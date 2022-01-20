34cea8130b86f3f23a1198ce81b73f5d80bff804
Mohd Aliyan <71941136+MohdAliyan27@users.noreply.github.com>
2022-01-20 18:53:46 +0530

    Update synamedia.js

--- synamedia.js
+++ synamedia.js
@@ -15,3 +15,32 @@ function split(string1){
 }
 
 split("abcabcabcdef");
+
+
+//Second way is this.
+
+//console.log("abcabcabcdef");
+
+function split(string1){
+  let count=0,counter=0;
+  let str2 =[...string1];
+  for(var i=0;i<str2.length;i++){
+  //console.log(str2.length);
+  counter = count;
+  if(str2[i]=="a"){
+      count=0;
+    	count++;
+  }
+  if(str2[i]!="a"){
+  count++;
+  }
+  
+  if(counter>count){
+  console.log(counter);
+  }
+  }
+  console.log(str2);
+  //console.log(counter);
+}
+split("abcabcabcdef");
+split("abcdefgabcabcdef");
