186bd2fbb888e03dff8775fdf6f2d0b8cb3e0b12
Mohd Aliyan <71941136+MohdAliyan27@users.noreply.github.com>
2022-01-24 11:50:58 +0530

    Even and Odd JS
    I need to find the even index and odd index from the string.

---
+++ EvenOddIndex.js
@@ -0,0 +1,34 @@
+function EvenOdd(word){
+    var x ="",y="";
+    for (var i=0;i<word.length;i++){
+        if(i%2==0){
+            x=x+word[i];
+        }
+    }
+    for (var j=0;j<word.length;j++){
+        if(j%2!=0){
+            y=y+word[j];
+        }
+    }
+    console.log(x+" "+y);
+}
+
+function processData(input) {
+    //Enter your code here
+    var pieces = input.split('\n');
+    for (var i = 0; i < parseInt(pieces[0]); i++) {
+        EvenOdd(pieces[i+1]);
+    }
+} 
+
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
