cd2efd6c57ea4d93523fc28760191de2c7321d12
Mohd Aliyan <71941136+MohdAliyan27@users.noreply.github.com>
2022-03-11 16:27:40 +0530

    Create Flattening.js

---
+++ Flattening.js
@@ -0,0 +1,12 @@
+//Flattening Array
+
+//Method 1
+var arr1= [ 1, 2, 3, [4, 5, [6, 7, [8], 9, 10], 11], 12, 13, 14, 15,[16,[17,18,19],20],21,22,23,24];
+console.log(arr1.flat(3));
+
+//Method 2
+var arr1= [ 1, 2, 3, [4, 5, [6, 7, [8], 9, 10], 11], 12, 13, 14, 15,[16,[17,18,19],20],21,22,23,24];
+let first = [].concat.apply([], arr1);
+let second = [].concat.apply([], first);
+let flatArray = [].concat.apply([], second);
+console.log(flatArray);
