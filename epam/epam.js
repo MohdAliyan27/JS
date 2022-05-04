7f82f54f5fd1c68273dfb4f0d043799a05b88a8d
Mohd Aliyan <71941136+MohdAliyan27@users.noreply.github.com>
2022-05-04 11:15:59 +0530

    Update epam.js

--- epam/epam.js
+++ epam/epam.js
@@ -5,11 +5,9 @@ var p = Promise.resolve('foo');
 async function callback(){ 
     let result = await p; 
     console.log(result); 
-
 } 
 
 callback(); 
-
 console.log(p.then(data => console.log('bar'))); 
 
 //Find output
@@ -22,5 +20,26 @@ function outer() {
     console.log(b); // Nan  --> 3
 
 } 
-
 outer(); 
+
+// Deepcopy shallow copy
+// Map function 
+// https://www.geeksforgeeks.org/check-for-balanced-parentheses-in-an-expression/
+// promises in javascript
+// https://www.geeksforgeeks.org/minimum-swaps-bracket-balancing/
+// https://www.geeksforgeeks.org/minimum-swaps-to-balance-the-given-brackets-at-any-index/
+
+//output
+function highestPowerOf2(n)
+{
+    return (n & (~(n - 1)));
+}
+ 
+var n = 48;
+console.log(highestPowerOf2(n));
+
+//hashing in node js
+//middleware
+//pm2 in node js
+//patch vs put HTTP
+//
