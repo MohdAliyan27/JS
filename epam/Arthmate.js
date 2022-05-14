089c274840392526c6a9f88fa73dea4ef7b48978
Mohd Aliyan <71941136+MohdAliyan27@users.noreply.github.com>
2022-05-14 17:54:45 +0530

    Create Arthmate.js

---
+++ epam/Arthmate.js
@@ -0,0 +1,10 @@
+1) Discuss some advantage or disadvantage of Node.js
+2) Event Loop
+3) Sync vs Async
+4) How Event Loop works
+5) Clustering
+6) Authentication
+7) How JWT works
+8)ACID properties
+9) Rollback vs commit
+10) 
