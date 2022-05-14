411b60399fc2851bec4fa68631fe2f3c441f3a5b
Mohd Aliyan <71941136+MohdAliyan27@users.noreply.github.com>
2022-05-14 18:08:29 +0530

    Update Arthmate.js

--- epam/Arthmate.js
+++ epam/Arthmate.js
@@ -4,7 +4,7 @@
 4) How Event Loop works
 5) Clustering
 6) Authentication
-7) How JWT works
+7) How JWT works payload and other details
 8) Full functionality JWT or OAuth.
 9) Rollback vs commit
 10) B2B vs B2C difference.
