ea4b4dfd07a39b90dab93308e32c96c2a976c5c0
Mohd Aliyan <71941136+MohdAliyan27@users.noreply.github.com>
2022-06-01 20:17:35 +0530

    Update Polygon.js

--- Polygon.js
+++ Polygon.js
@@ -11,6 +11,7 @@ class Polygon{
     } 
 }
 
+//Function call
 const rectangle = new Polygon([10, 20, 10, 20]);
 const square = new Polygon([10, 10, 10, 10]);
 const pentagon = new Polygon([10, 20, 30, 40, 43]);
