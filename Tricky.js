21b93efe89972fc8256396e66f15ca452f724d50
Mohd Aliyan <71941136+MohdAliyan27@users.noreply.github.com>
2022-06-08 22:52:22 +0530

    Update Tricky.js

--- Tricky.js
+++ Tricky.js
@@ -18,3 +18,16 @@ THEORY and Concepts
 var x; // declaring x
 console.log(x); //output: undefined
 console.log(y);  //output : not defined
+
+//TYPE COERSION
+var x = 220;
+var y = "Hello";
+var z = undefined;
+        
+if( x && y ){ 
+    console.log("Code runs 2" ); // This block runs because x && y returns "Hello" (Truthy)
+  }   
+          
+  if( x && z ){
+    console.log("Code runs 1");  // This block runs because x || y returns 220(Truthy)
+  }
