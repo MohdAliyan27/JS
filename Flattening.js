880a0c0f926ee953edef211fc1266bbfac8f4669
Mohd Aliyan <71941136+MohdAliyan27@users.noreply.github.com>
2022-06-09 23:55:18 +0530

    Update Flattening.js

--- Flattening.js
+++ Flattening.js
@@ -7,6 +7,7 @@ console.log(arr1.flat(3));
 //End of function
 
 //Method 2
+// . Concat.apply
 var arr1= [ 1, 2, 3, [4, 5, [6, 7, [8], 9, 10], 11], 12, 13, 14, 15,[16,[17,18,19],20],21,22,23,24];
 let first = [].concat.apply([], arr1);
 let second = [].concat.apply([], first);
